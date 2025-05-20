28c8fb17ec07d1a20787b5c5d99dfd7402c6ae46
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-05-19 21:35:22 -0400

    Committing work for submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // console.log("JS LOADED");
   // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Avoid page refresh
+
+    const taskInput = input.value;
+    // console.log("Task Input: ", taskInput);
+    buildToDo(taskInput);
+    form.reset(); // Clear input after submission
+  });
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    document.getElementById("tasks").appendChild(li);
+    // console.log("Append new task: ", li);
+  }
 });
